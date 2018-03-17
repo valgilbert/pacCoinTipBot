d8348add01a98c1df36a232383fff2b12f61595e
valgilbert <vgl_19@yahoo.com>
2018-03-17 20:40:04 +1100

    Update install.sh
    Update to PacCoin

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ sudo apt-get install python3 python3-pip
 sudo pip3 install python-telegram-bot --upgrade
 sudo pip3 install discord.py --upgrade
 
-sudo mkdir -p /opt/lynxTipBot
+sudo mkdir -p /opt/pacCoinTipBot
 
-sudo cp -prf ~/lynxTipBot/lynxDiscordTipBot /etc/init.d/lynxDiscordTipBot
-sudo cp -prf ~/lynxTipBot/lynxDiscordTipBot.py /opt/lynxTipBot/lynxDiscordTipBot.py
-sudo cp -prf ~/lynxTipBot/lynxTelegramTipBot /etc/init.d/lynxTelegramTipBot
-sudo cp -prf ~/lynxTipBot/lynxTelegramTipBot.py /opt/lynxTipBot/lynxTelegramTipBot.py
-sudo cp -prf ~/lynxTipBot/pickledb.py /opt/lynxTipBot/pickledb.py
+sudo cp -prf ~/pacCoinTipBot/pacCoinDiscordTipBot /etc/init.d/pacCoinDiscordTipBot
+sudo cp -prf ~/pacCoinTipBot/pacCoinDiscordTipBot.py /opt/pacCoinTipBot/pacCoinDiscordTipBot.py
+sudo cp -prf ~/pacCoinTipBot/pacCoinTelegramTipBot /etc/init.d/pacCoinTelegramTipBot
+sudo cp -prf ~/pacCoinTipBot/pacCoinTelegramTipBot.py /opt/pacCoinTipBot/pacCoinTelegramTipBot.py
+sudo cp -prf ~/pacCoinTipBot/pickledb.py /opt/pacCoinTipBot/pickledb.py
 
-rm -rf ~/lynxTipBot
+rm -rf ~/pacCoinTipBot
 
 echo "DONE!!"
